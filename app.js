54d0a5e65b3a8f4b2b455a69434681fc1b1cc504
Anse98 <lorenzo.anselmi98.com>
2023-10-24 14:37:30 +0200

    ADD-Carousel with Vue

---
+++ app.js
@@ -0,0 +1,40 @@
+
+//VUE
+
+const { createApp } = Vue
+
+  createApp({
+    data() {
+      return {
+        currentIndex: 0,
+
+        images: [
+          "img/01.webp",
+          "img/02.webp",
+          "img/03.webp",
+          "img/04.webp",
+          "img/05.webp"
+        ],  
+      }
+    },
+    methods: {
+      nextSlide(){
+        this.currentIndex++;
+
+        if(this.currentIndex === this.images.length) {
+          this.currentIndex = 0;
+        }
+      },
+
+      prevSlide(){
+        this.currentIndex--;
+
+        if(this.currentIndex < 0) {
+          this.currentIndex = this.images.length - 1;
+        }
+      }
+    }
+  }).mount('#app')
+
+  
+  
